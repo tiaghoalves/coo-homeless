22f4a208f311576a3ea2cfbf50789cc25e1f3cef
Tiago Alves <tiago.alves718@gmail.com>
2017-09-21 01:58:12 -0300

    Create repository - creating the localizationRepository

---
+++ app/src/main/java/com/coohomeless/repository/LocalizationRepository.java
@@ -0,0 +1,11 @@
+package com.coohomeless.repository;
+
+import com.coohomeless.models.LocalizationModel;
+import com.strongloop.android.loopback.ModelRepository;
+
+public class LocalizationRepository extends ModelRepository<LocalizationModel> {
+
+    public LocalizationRepository() {
+        super("localization", "localizations", LocalizationModel.class);
+    }
+}
