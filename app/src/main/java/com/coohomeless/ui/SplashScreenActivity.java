a3fefb3f0027441700c9b995b69a4452216cf82a
Tiago Alves <tiago.alves718@gmail.com>
2017-10-14 04:09:16 -0300

    Add splash screen

---
+++ app/src/main/java/com/coohomeless/ui/SplashScreenActivity.java
@@ -0,0 +1,33 @@
+package com.coohomeless.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.text.InputType;
+
+import com.coohomeless.R;
+
+public class SplashScreenActivity extends Activity {
+
+    private static final int SPLASH_SCREEN_DELAY = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showLoginScreen();
+                finish();
+            }
+        }, SPLASH_SCREEN_DELAY);
+    }
+
+    public void showLoginScreen() {
+        Intent toLogin = new Intent(SplashScreenActivity.this, LoginActivity.class);
+        startActivity(toLogin);
+    }
+}
