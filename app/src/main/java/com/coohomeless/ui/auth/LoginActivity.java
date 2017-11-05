c86eeadb8f5e72dca1d3fdeb0ac274aa678d70bb
Tiago Alves <tiago.alves718@gmail.com>
2017-11-05 00:55:20 -0200

    Google SignIn Auth ?

--- app/src/main/java/com/coohomeless/ui/auth/LoginActivity.java
+++ app/src/main/java/com/coohomeless/ui/auth/LoginActivity.java
@@ -1,43 +1,49 @@
 package com.coohomeless.ui.auth;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.Patterns;
-import android.view.KeyEvent;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.coohomeless.R;
-import com.coohomeless.models.user.UserModel;
 import com.coohomeless.ui.MenuActivity;
+import com.coohomeless.ui.SplashScreenActivity;
 import com.dd.processbutton.iml.ActionProcessButton;
-import com.google.firebase.auth.FirebaseAuth;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseUser;
-import com.strongloop.android.loopback.User;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends BaseAuthActivity implements
+        View.OnClickListener,
+        GoogleApiClient.OnConnectionFailedListener {
+
     private static final String TAG = "LoginActivity";
     private static final int REQUEST_SIGNUP = 0;
+    private static final int RC_SIGN_IN = 9001;
 
-    /* A reference to the Firebase */
-    private FirebaseAuth mAuth;
-
-    /* Listener for Firebase session changes */
-    private FirebaseAuth.AuthStateListener mAuthListener;
+    private GoogleApiClient mGoogleApiClient;
 
     @BindView(R.id.input_email) EditText inputEmail;
     @BindView(R.id.input_password) EditText inputPassword;
     @BindView(R.id.btn_login) ActionProcessButton btnLogin;
     @BindView(R.id.btn_signup) ActionProcessButton btnSignUp;
+    @BindView(R.id.sign_in_google) SignInButton btnSignInGoogle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -45,90 +51,28 @@ public class LoginActivity extends Activity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
-        mAuth = FirebaseAuth.getInstance();
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed_out");
-                }
-                // ...
-            }
-        };
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btnLogin.setMode(ActionProcessButton.Mode.ENDLESS);
-                btnLogin.setProgress(1);
+        btnLogin.setOnClickListener(this);
+        btnSignUp.setOnClickListener(this);
+        btnSignInGoogle.setOnClickListener(this);
 
-                login();
-            }
-        });
-
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                btnSignUp.setMode(ActionProcessButton.Mode.ENDLESS);
-                btnSignUp.setProgress(1);
-
-                // Start the Signup activity
-                Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
-                startActivityForResult(intent, REQUEST_SIGNUP);
-            }
-        });
-
+        mAuth = super.getmAuth();
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-
-    public void login() {
-        if (!validate()) {
-            onLoginFailed();
-            return;
-        }
-
-        btnLogin.setEnabled(false);
-
-        // TODO: Implement your own authentication logic here.
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                String email = inputEmail.getText().toString();
-                String password = inputPassword.getText().toString();
-
-                // On complete call either onLoginSuccess or onLoginFailed
-                if(email.isEmpty() || password.isEmpty()) {
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials", Toast.LENGTH_SHORT).show();
-                    onLoginFailed();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Redirecting...", Toast.LENGTH_SHORT).show();
-                    onLoginSuccess();
-                }
-            }
-        }, 3000);
-    }
-
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
         if (requestCode == REQUEST_SIGNUP) {
             if (resultCode == RESULT_OK) {
 
@@ -136,6 +80,23 @@ public class LoginActivity extends Activity {
                 // By default we just finish the Activity and log them in automatically
                 this.finish();
             }
+        } else if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            Toast.makeText(getApplicationContext(),
+                    "Login Google Realizado: " + acct.getDisplayName(), Toast.LENGTH_SHORT).show();
+            onLoginSuccess();
+        } else {
+            // Signed out, show unauthenticated UI.
+            logout();
         }
     }
 
@@ -143,6 +104,34 @@ public class LoginActivity extends Activity {
     public void onBackPressed() {
         // disable going back to the MainActivity
         moveTaskToBack(true);
+    }
+
+    public void login(String email, String password) {
+        if (!validate(email, password)) {
+            Toast.makeText(getApplicationContext(), "Login inválido", Toast.LENGTH_SHORT).show();
+            onLoginFailed();
+            return;
+        }
+        btnLogin.setEnabled(false);
+
+        // TODO: Implement your own authentication logic
+        // On complete call either onLoginSuccess or onLoginFailed
+        Toast.makeText(getApplicationContext(), "Redirecionando...", Toast.LENGTH_SHORT).show();
+        onLoginSuccess();
+    }
+
+    private void logout() {
+        // Firebase sign out
+        mAuth.signOut();
+
+        // Google sign out
+        Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback(
+                new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(@NonNull Status status) {
+                        Toast.makeText(getApplicationContext(), "Usuario deslogado", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     public void onLoginSuccess() {
@@ -156,20 +145,23 @@ public class LoginActivity extends Activity {
         btnLogin.setEnabled(true);
     }
 
-    public boolean validate() {
+    public boolean validate(String email, String password) {
         boolean valid = true;
 
-        String email = inputEmail.getText().toString();
-        String password = inputPassword.getText().toString();
-
-        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        if (email.isEmpty() ) {
             inputEmail.setError("endereço de e-mail inválido");
+            valid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            inputEmail.setError("obrigatório");
             valid = false;
         } else {
             inputEmail.setError(null);
         }
 
-        if (password.isEmpty() || password.length() < 6) {
+        if (password.isEmpty()) {
+            inputPassword.setError("obrigatório");
+            valid = false;
+        } else if (password.length() < 6) {
             inputPassword.setError("pelo menos 6 caracteres");
             valid = false;
         } else {
@@ -179,4 +171,36 @@ public class LoginActivity extends Activity {
         return valid;
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btn_login) {
+            btnLogin.setMode(ActionProcessButton.Mode.ENDLESS);
+            btnLogin.setProgress(1);
+            login(inputEmail.getText().toString(), inputPassword.getText().toString());
+
+        } else if (id == R.id.btn_signup) {
+            btnSignUp.setMode(ActionProcessButton.Mode.ENDLESS);
+            btnSignUp.setProgress(1);
+            // Start the Signup activity
+            Intent toSignUp = new Intent(getApplicationContext(), SignUpActivity.class);
+            startActivityForResult(toSignUp, REQUEST_SIGNUP);
+
+        } else if (id == R.id.sign_in_google) {
+            Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+            startActivityForResult(signInIntent, RC_SIGN_IN);
+
+        } else if (id == R.id.input_email || id == R.id.input_password) {
+            btnLogin.setProgress(0);
+            btnSignUp.setProgress(0);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/coohomeless/ui/auth/SignUpActivity.java
+++ app/src/main/java/com/coohomeless/ui/auth/SignUpActivity.java
@@ -1,8 +1,8 @@
 package com.coohomeless.ui.auth;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,12 +15,18 @@ import android.widget.Toast;
 import com.coohomeless.R;
 import com.coohomeless.models.user.UserModel;
 import com.coohomeless.ui.MenuActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "SignUpActivity";
+
+    private FirebaseAuth mAuth;
 
     @BindView(R.id.input_name) EditText _nameText;
     @BindView(R.id.input_email) EditText _emailText;
@@ -34,22 +40,16 @@ public class SignUpActivity extends AppCompatActivity {
         setContentView(R.layout.activity_signup);
         ButterKnife.bind(this);
 
-        _signupButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signup();
-            }
-        });
+        _signupButton.setOnClickListener(this);
+        _loginLink.setOnClickListener(this);
 
-        _loginLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Finish the registration screen and return to the Login activity
-                Intent toLogin = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(toLogin);
-                finish();
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
     }
 
     public void signup() {
@@ -60,39 +60,49 @@ public class SignUpActivity extends AppCompatActivity {
 
         _signupButton.setEnabled(false);
 
-        final ProgressDialog progressDialog = new ProgressDialog(SignUpActivity.this, R.style.Theme_AppCompat_DayNight_Dialog);
-        progressDialog.setIndeterminate(true);
-        progressDialog.setMessage("Creating Account...");
-        progressDialog.show();
-
 
         // TODO: Implement your own signup logic here.
+        String name = _nameText.getText().toString();
+        String email = _emailText.getText().toString();
+        String password = _passwordText.getText().toString();
 
-        new Handler().postDelayed(
-                new Runnable() {
-                    public void run() {
-                        String name = _nameText.getText().toString();
-                        String email = _emailText.getText().toString();
-                        String password = _passwordText.getText().toString();
+        // On complete call either onSignupSuccess or onSignupFailed
+        // depending on success
+        UserModel user = new UserModel();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setFullName(name);
 
-                        // On complete call either onSignupSuccess or onSignupFailed
-                        // depending on success
-                        UserModel user = new UserModel();
-                        user.setEmail(email);
-                        user.setPassword(password);
-                        user.setFullName(name);
-
-                        onSignupSuccess(user);
-                        // onSignupFailed();
-                        progressDialog.dismiss();
-                    }
-                }, 3000);
+        onSignupSuccess(user);
     }
 
+    private void sendEmailVerification() {
+        // Send verification email
+        final FirebaseUser user = mAuth.getCurrentUser();
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // [START_EXCLUDE]
+
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(SignUpActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
     public void onSignupSuccess(UserModel user) {
         _signupButton.setEnabled(true);
         setResult(RESULT_OK, null);
+        sendEmailVerification();
 
         Intent toLogin = new Intent(SignUpActivity.this, LoginActivity.class);
         Bundle bundle = new Bundle();
@@ -140,4 +150,16 @@ public class SignUpActivity extends AppCompatActivity {
         return valid;
     }
 
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.btn_signup) {
+            signup();
+        } else if (id == R.id.link_login){
+            // Finish the registration screen and return to the Login activity
+            Intent toLogin = new Intent(SignUpActivity.this, LoginActivity.class);
+            startActivity(toLogin);
+            finish();
+        }
+    }
 }
