accaee299ad95436d999bf0a882b9c79fe772509
Tiago Alves <tiago.alves718@gmail.com>
2017-11-05 00:54:10 -0200

    Add play-services-auth dependency

---
+++ app/src/main/java/com/coohomeless/ui/auth/BaseAuthActivity.java
@@ -0,0 +1,60 @@
+package com.coohomeless.ui.auth;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class BaseAuthActivity extends AppCompatActivity {
+
+    private final String TAG = "BaseAuth";
+
+    /* A reference to the Firebase */
+    protected FirebaseAuth mAuth;
+
+    /* Listener for Firebase session changes */
+    protected FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public FirebaseAuth getmAuth() {
+        return mAuth;
+    }
+}
