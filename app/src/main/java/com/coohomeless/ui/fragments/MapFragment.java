ef6587aa2db02abc5be8ac514ff62cb1da05371f
Tiago Alves <tiago.alves718@gmail.com>
2017-11-07 01:28:15 -0200

    Build passed - Add MapFragment - Add Mapview layout - Add more user-permissions on Manifest

--- app/src/main/java/com/coohomeless/ui/fragments/MapFragment.java
+++ app/src/main/java/com/coohomeless/ui/fragments/MapFragment.java
@@ -1,7 +1,85 @@
 package com.coohomeless.ui.fragments;
 
 import android.app.Fragment;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MapFragment extends Fragment {
+import com.coohomeless.Manifest;
+import com.coohomeless.R;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    MapView mapView;
+    GoogleMap mGoogleMap;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+
+        // Gets the MapView from the XML layout and creates it
+        mapView = (MapView) v.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+
+        // Gets to GoogleMap from the MapView and does initialization stuff
+        mapView.getMapAsync(this);
+        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        if (checkExternalPermission()) {
+            mGoogleMap.setMyLocationEnabled(true);
+        }
+
+        // Needs to call MapsInitializer before doing any CameraUpdateFactory calls
+        MapsInitializer.initialize(this.getActivity());
+
+        // Updates the location and zoom of the MapView
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(43.1, -87.9), 10);
+        mGoogleMap.animateCamera(cameraUpdate);
+
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(-29.843504, -51.13166)));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-29.843504, -51.13166), 10));
+    }
+
+    @Override
+    public void onResume() {
+        mapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    private boolean checkExternalPermission() {
+        int permissionCheck = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.MAPS_RECEIVE);
+        return  (permissionCheck == PackageManager.PERMISSION_GRANTED);
+    }
 
 }
